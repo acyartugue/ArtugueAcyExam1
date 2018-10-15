98ce5b3bf432d804efb833b2190d1792d5c4db76
unknown <ARTUGUE_ACY_115@IICSGENERAL.com>
2018-10-15 16:43:26 +0800

    Initial commit

---
+++ app/src/main/java/com/example/artugue_acy_115/artugueacyexam1/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.artugue_acy_115.artugueacyexam1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
